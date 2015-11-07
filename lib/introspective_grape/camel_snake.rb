a4e3746a92b0259fde6dd179cdc916dc721bb5da
josh.buermann <josh.buermann@springshot.com>
2015-11-06 16:02:40 -0800

    Include the duck typing for camel/snake case agnosticism.

--- lib/introspective_grape/camel_snake.rb
+++ lib/introspective_grape/camel_snake.rb
@@ -20,3 +20,51 @@ module IntrospectiveGrape::CamelSnake
     end
   end
 end
+
+# Duck type Grape's built in JSON formatter to convert all snake case hash keys
+# to camel case.
+module Grape
+  module Formatter
+    module Json
+      class << self
+        include IntrospectiveGrape::CamelSnake
+        def call(object, env)
+          if object.respond_to?(:to_json)
+            camel_keys(JSON.parse(object.to_json)).to_json
+         else
+            camel_keys(MultiJson.dump(object)).to_json
+          end
+        end
+      end
+    end
+  end
+end
+
+# Camelize the parameters in the swagger documentation.
+module Grape
+  class API
+    class << self
+      private
+      def create_documentation_class_with_camelized
+        doc = create_documentation_class_without_camelized
+        doc.class_eval do
+          class << self
+            def parse_params_with_camelized(params, path, method)
+              parsed_params = parse_params_without_camelized(params, path, method)
+              parsed_params.each_with_index do |param|
+                param[:name] = param[:name]
+                               .camelize(:lower)
+                               .gsub(/\[Destroy\]/,'[_destroy]')
+              end
+              parsed_params
+            end
+
+            alias_method_chain :parse_params, :camelized
+          end
+        end
+        doc
+      end
+      alias_method_chain :create_documentation_class, :camelized
+    end
+  end
+end
