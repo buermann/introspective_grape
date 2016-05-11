2ef2bef9625919fb61318890f5972bcb11d3df99
jbuermann <josh.buermann@springshot.com>
2016-05-11 15:17:53 -0700

    Autoload camel-json

--- lib/introspective_grape.rb
+++ lib/introspective_grape.rb
@@ -2,7 +2,9 @@ module IntrospectiveGrape
   autoload :API,        'introspective_grape/api'
   autoload :Helpers,  'introspective_grape/helpers'
   autoload :CamelSnake, 'introspective_grape/camel_snake'
-  autoload :Formatter, 'introspective_grape/formatter/camel_json'
+  module Formatter
+    autoload :CamelJson, 'introspective_grape/formatter/camel_json'
+  end
 
   def self.config
     @config = OpenStruct.new(camelize_parameters: true)
