28d4d46b3d34db21f8e790dfafb9778a7bff12fb
Aryk Grosz <aryk.grosz@gmail.com>
2017-02-27 20:19:25 +0200

    Fix camel json formatter

--- lib/introspective_grape/formatter/camel_json.rb
+++ lib/introspective_grape/formatter/camel_json.rb
@@ -1,14 +1,16 @@
 # Add a formatter to grape that converts all snake case hash keys from ruby to camel case.
 require 'camel_snake_keys'
+require 'grape/formatter/json'
 module IntrospectiveGrape
   module Formatter
     module CamelJson
       def self.call(object, _env)
-        if object.respond_to?(:to_json)
-          JSON.parse(object.to_json).with_camel_keys.to_json
-        else
-          MultiJson.dump(object).with_camel_keys.to_json
+        if object.respond_to?(:to_json) && !object.respond_to?(:with_camel_keys) &&
+          (parsed_object = JSON.parse(object.to_json)).respond_to?(:with_camel_keys)
+          object = parsed_object
         end
+        object = object.with_camel_keys if object.respond_to?(:with_camel_keys)
+        Grape::Formatter::Json.call(object, _env)
       end
     end
   end
