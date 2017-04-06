499d86461fa826debf6965d29b302d328bb68b2e
Joshua Buermann <buermann@gmail.com>
2017-04-06 11:57:05 -0700

    Minimize active support dependencies in camel_snake.rb

--- lib/introspective_grape/camel_snake.rb
+++ lib/introspective_grape/camel_snake.rb
@@ -1,5 +1,6 @@
 require 'grape-swagger'
-require 'active_support' #/core_ext/module/aliasing'
+require 'active_support/core_ext/module/aliasing'
+require 'active_support/inflector/methods'
 require 'camel_snake_keys'
 if IntrospectiveGrape.config.camelize_parameters 
   # Camelize the parameters in the swagger documentation.
